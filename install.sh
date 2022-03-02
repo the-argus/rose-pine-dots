1ef2cf64701537c730206ebde518d76bc4b2ce7b
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-03-01 19:32:55 -0500

    added rose pine gtk install

--- install.sh
+++ install.sh
@@ -36,7 +36,24 @@ Server = https://download.opensuse.org/repositories/home:/ungoogled_chromium/Arc
 sudo pacman -Syu
 
 # install all my packages
+echo "INSTALLING ALL SOFTWARE"
 sudo pacman -S $(cat paclist)
+
+
+echo "INSTALLING ROSE-PINE-GTK"
+wget https://github.com/rose-pine/gtk/releases/download/v2.0.0/AllRosePineThemesGTK.tar.gz
+wget https://github.com/rose-pine/gtk/releases/download/v2.0.0/AllRosePineThemesIcons.tar.gz
+
+tar xf AllRosePineThemesGTK.tar.gz
+tar xf AllRosePineThemesIcons.tar.gz
+rm AllRosePineThemesGTK.tar.gz
+rm AllRosePineThemesIcons.tar.gz
+
+sudo cp -r AllRosePineThemesGTK/rose-pine-gtk /usr/share/themes
+sudo cp -r AllRosePineThemesIcons/rose-pine-icons /usr/share/icons
+
+rm -rf AllRosePineThemesGTK
+rm -rf AllRosePineThemesIcons
 
 # reminder
 echo 'reminders for myself:
